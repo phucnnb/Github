8b4b9644e7dd6a39a1bbc4fbdc4b03dff4c066f4
Phuc <phucnnb@paxcreation.com>
2019-04-23 17:05:34 +0700

    add more

--- demoGG/app/src/main/java/com/example/demogg/View/Welcome/Welcome.java
+++ demoGG/app/src/main/java/com/example/demogg/View/Welcome/Welcome.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,18 +18,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.demogg.Presenter.Welcome.PresenterLogicWelcome;
 import com.example.demogg.R;
 import com.example.demogg.View.MainActivity.MainActivity;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class Welcome extends AppCompatActivity implements ViewWelcome, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class Welcome extends AppCompatActivity implements ViewWelcome, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener {
 
     @BindView(R.id.btnChao)
     Button btnChao;
@@ -38,6 +42,7 @@ public class Welcome extends AppCompatActivity implements ViewWelcome, GoogleApi
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private Location location;
     private SharedPreferences share;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,14 +72,14 @@ public class Welcome extends AppCompatActivity implements ViewWelcome, GoogleApi
         }else {
             googleApiClient.connect();
         }
-    }
 
+    }
 
     private void event() {
         btnChao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Check();
+                googleApiClient.connect();
                 logicWelcome.ThucHienChuyenActivity(location, share);
 
 
@@ -120,7 +125,14 @@ public class Welcome extends AppCompatActivity implements ViewWelcome, GoogleApi
     @SuppressLint("MissingPermission")
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
         location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if(location == null){
+            logicWelcome.ThucHienShowAlert();
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,mLocationRequest,this);
+        }
         //Log.d("AAA",location.toString());
     }
 
@@ -143,5 +155,10 @@ public class Welcome extends AppCompatActivity implements ViewWelcome, GoogleApi
                     googleApiClient.connect();
                 }break;
         }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 }
