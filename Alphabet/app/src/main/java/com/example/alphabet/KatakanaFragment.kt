7d81d1ed6bd76b4d419620a93ca963366fceecc8
Phuc <phucnnb@paxcreation.com>
2019-09-19 16:01:47 +0700

    aaa

--- Alphabet/app/src/main/java/com/example/alphabet/KatakanaFragment.kt
+++ Alphabet/app/src/main/java/com/example/alphabet/KatakanaFragment.kt
@@ -12,7 +12,6 @@ class KatakanaFragment : Fragment(){
 
     private var checkTypeFace : Boolean = false
     private lateinit var viewButtonKata: LinearLayout
-     
     private lateinit var imageTypeFaceKata : ImageView
     private lateinit var btnRollKata : Button
     private lateinit var tvCountKata : TextView
